716abbf009735cbe488eab5155b3dbdb80d35c80
Çağatay ÇALI <me@calicagatay.me>
2016-07-25 11:53:20 -0700

    Readme improved

--- demo.js
+++ demo.js
@@ -1,13 +1,3 @@
-# Installation
-
-```
-sudo npm i phone-number-fixer --save
-```
-
-# Using
-
-| In demo.js
-```
 var phoneNumberFixer = require('phone-number-fixer');
 
 var number = '05054146201';
@@ -15,4 +5,3 @@ var number = '05054146201';
 console.log(phoneNumberFixer.check(number, 'TR'));
 
 console.log(phoneNumberFixer.suggestion(number));
-``
